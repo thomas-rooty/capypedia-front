a08cbeb1ac8a0a4ad1a02d278b39cd96ba577dbf
thomas-rooty <tc.caron91@gmail.com>
2023-06-28 21:48:21 +0200

    Register page theme

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,15 +1,14 @@
 'use client'
 import styles from './page.module.css'
 import Register from '@/components/forms/register/Register'
+import CapyWalking from '@/components/capywalking/CapyWalking'
 
 const RegisterPage = () => {
   return (
-    <div className={styles.container}>
+    <main className={styles.main}>
       <Register />
-      <header className={styles.header}>
-        <img src={'/images/header_blurred.png'} className={styles.headerImage} alt="Header background image" />
-      </header>
-    </div>
+      <CapyWalking />
+    </main>
   )
 }
 
