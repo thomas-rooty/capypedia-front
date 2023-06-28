9fa7ef1fabb4d734e3ed94d77ecca8b6b810ebc3
thomas-rooty <tc.caron91@gmail.com>
2023-06-28 16:26:47 +0200

    Header style done

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,6 +4,14 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <img src={'/images/header_blurred.png'} className={styles.headerImage} alt="Header background image" />
+      <div className={styles.headerContent}>
+        <h1 className={styles.headerTitle}>
+          Bienvenue sur Capypedia, l'encyclopédie des capybaras.
+        </h1>
+        <p className={styles.headerDescription}>
+          Vous trouverez ici toutes les informations nécessaires pour tout savoir sur ces adorables rongeurs.
+        </p>
+      </div>
     </header>
   )
 }
