a177b88bcb20c3eb8ec560bc14d48bde64dba0da
thomas-rooty <tc.caron91@gmail.com>
2023-06-29 16:26:27 +0200

    Only show approved observations to users

--- components/observations/AllObservations.tsx
+++ components/observations/AllObservations.tsx
@@ -1,22 +1,46 @@
 'use client'
 import styles from './AllObservations.module.css'
-import { getObservations } from '@/utils/apiUtils'
+import { getObservations, checkRole } from '@/utils/apiUtils'
 import { useState, useEffect } from 'react'
 import { Observation } from '@/utils/types/Observation'
+import { getUserData } from '@/utils/mutations/storageMutations'
 import CardObservation from '@/components/cards/CardObservation'
 
 const AllObservations = () => {
+  const [role, setRole] = useState<string>('')
   const [observations, setObservations] = useState<Observation[]>([])
   const [loading, setLoading] = useState<boolean>(true)
 
+  // Check user role
+  useEffect(() => {
+    const fetchRole = async () => {
+      const { token } = await getUserData()
+      const res = await checkRole(token)
+      if (res.status === 401) {
+        setRole('user')
+      }
+      if (res.status === 200) {
+        setRole('admin')
+      }
+    }
+    fetchRole()
+  }, [])
+
+  // Get observations
   useEffect(() => {
     const fetchObservations = async () => {
-      const observations = await getObservations()
-      setObservations(observations.data)
+      const fetchedObservations = await getObservations()
+      console.log(fetchedObservations)
+      if (role === 'admin') {
+        setObservations(fetchedObservations.data)
+      } else {
+        const userObservations = fetchedObservations.data.filter((observation: Observation) => observation.approved)
+        setObservations(userObservations)
+      }
       setLoading(false)
     }
     fetchObservations()
-  }, [])
+  }, [role])
 
   return (
     <div className={styles.container}>
@@ -25,7 +49,7 @@ const AllObservations = () => {
       ) : (
         <div className={styles.grid}>
           {observations.map((observation) => (
-              <CardObservation observation={observation} key={observation.id} />
+            <CardObservation observation={observation} key={observation.id} />
           ))}
         </div>
       )}
