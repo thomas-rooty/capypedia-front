ca01cef509bb6e0ec3e7eb5ac94255811eabe9db
thomas-rooty <tc.caron91@gmail.com>
2023-06-30 09:26:46 +0200

    Le pain on walking capybara

--- components/capywalking/CapyWalking.tsx
+++ components/capywalking/CapyWalking.tsx
@@ -1,10 +1,20 @@
+'use client'
 import styles from './CapyWalking.module.css'
 
 const CapyWalking = () => {
+  // Play sound on click
+  const playSound = () => {
+    const audio = document.getElementById('lepain') as HTMLAudioElement
+    audio.play()
+  }
+
   return (
-    <div className={styles.capyWalking}>
-      <img className={styles.capyGif} src="/icons/capybara_walking.gif" alt="Capybara walking" />
-    </div>
+    <>
+      <div className={styles.capyWalking}>
+        <img className={styles.capyGif} src="/icons/capybara_walking.gif" alt="Capybara walking" onClick={playSound} />
+      </div>
+      <audio id="lepain" className={styles.capySound} src="/sounds/le_pain.mp3" />
+    </>
   )
 }
 
